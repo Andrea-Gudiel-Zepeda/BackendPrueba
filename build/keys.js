96e93ebf24eabf8aea8354cf664e09f794f292a7
Andrea-Gudiel-Zepeda <[andreamaria1d@gmail.com]>
2022-07-12 01:24:03 -0600

    base de datos

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'connect_url'
+        host: 'us-cdbr-east-06.cleardb.net',
+        user: 'b67a4c29a45208',
+        password: 'edd06b5c',
+        database: 'heroku_ec54699c21e79a4'
     }
 };
