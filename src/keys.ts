7c4cf76a893ed02f0f5b21dc865973dc98e36160
Andrea-Gudiel-Zepeda <[andreamaria1d@gmail.com]>
2022-07-12 10:13:38 -0600

    que funcioneee

--- src/keys.ts
+++ src/keys.ts
@@ -1,10 +1,10 @@
 export default {
 
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'connect_url'
+        host: 'lus-cdbr-east-06.cleardb.net',
+        user: 'rb67a4c29a45208',
+        password: 'dd06b5c',
+        database: 'heroku_ec54699c21e79a4'
     }
 
 }
